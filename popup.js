2f01b11e8a9577e7f3804b85841f47f1375fc1f9
Sean Gershman <sgershman@uchicago.edu>
2025-02-28 14:31:58 -0600

    summary page

--- popup.js
+++ popup.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const greetingButton = document.getElementById('greetButton');
+  const greetingButton = document.getElementById('yesButton');
 
   greetingButton.addEventListener('click', () => {
       // Send a message to the background script
-      chrome.runtime.sendMessage({ type: 'GREETING', greeting: 'Hello, background!' }, (response) => {
+      //chrome.runtime.sendMessage({ type: 'GREETING', greeting: 'Hello, background!' }, (response) => {
           // Display an alert with the response message
-          alert('Response from background: ' + response.response);
-      });
+      //    alert('Response from background: ' + response.response);
+      //});
+
+      window.location.href = 'summary-page.html';
+
   });
 });
 //test
