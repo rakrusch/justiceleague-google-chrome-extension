6270702b8101bfb045717e66f1d5c80577bc71cb
Ryan Krusch <rakrusch@gmail.com>
2025-03-01 14:43:08 -0600

    translations for popup page

--- popup.js
+++ popup.js
@@ -1,32 +1,56 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const yesButton = document.getElementById('yesButton');
-  const noButton = document.getElementById('noButton');
-  const closeButton = document.getElementById('closeButton');
-  const settingsButton = document.getElementById('settingsButton');
+document.addEventListener("DOMContentLoaded", function () {
+  const yesButton = document.getElementById("yesButton");
+  const noButton = document.getElementById("noButton");
+  const closeButton = document.getElementById("closeButton");
+  const settingsButton = document.getElementById("settingsButton");
 
+  yesButton.addEventListener("click", () => {
+    // Send a message to the background script
+    //chrome.runtime.sendMessage({ type: 'GREETING', greeting: 'Hello, background!' }, (response) => {
+    // Display an alert with the response message
+    //    alert('Response from background: ' + response.response);
+    //});
 
-
-  yesButton.addEventListener('click', () => {
-      // Send a message to the background script
-      //chrome.runtime.sendMessage({ type: 'GREETING', greeting: 'Hello, background!' }, (response) => {
-          // Display an alert with the response message
-      //    alert('Response from background: ' + response.response);
-      //});
-      
-      window.location.href = 'summary-page.html';
-
+    window.location.href = "summary-page.html";
   });
 
-  noButton.addEventListener('click', () => {
+  noButton.addEventListener("click", () => {
     window.close();
   });
 
-  closeButton.addEventListener('click', () => {
+  closeButton.addEventListener("click", () => {
     window.close();
   });
 
-  settingsButton.addEventListener('click', () => {
-    window.location.href = 'settings-page.html';
+  settingsButton.addEventListener("click", () => {
+    window.location.href = "settings-page.html";
   });
 
+  const savedLanguage = localStorage.getItem("selectedLanguage") || "English";
+  applyTranslation(savedLanguage);
 });
+
+const translations = {
+  English: {
+    title: "Would you like to summarize this privacy policy?",
+    yes: "Yes", 
+    no: "No"
+  },
+  Arabic: {
+    title: "هل ترغب في تلخيص سياسة الخصوصية هذه؟",
+    yes: "نعم",
+    no:"لا"
+  },
+  Swahili: {
+    title: "Je, ungependa kufanya muhtasari wa sera hii ya faragha?",
+    yes: "ndio",
+    no: "hapana"
+  },
+};
+function applyTranslation(language) {
+  const translation = translations[language];
+  document.getElementById("titleQ").textContent = translation.title;
+  document.getElementById("yesButton").textContent = translation.yes;
+  document.getElementById("noButton").textContent = translation.no;
+
+}
