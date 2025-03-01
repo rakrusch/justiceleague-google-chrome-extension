cd83f0eacd6f9e0ff4da29976a8b6d8addc0a426
Ryan Krusch <rakrusch@gmail.com>
2025-03-01 14:52:09 -0600

    translation stuff

--- settings-page.js
+++ settings-page.js
@@ -1,17 +1,16 @@
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  const closeButton = document.getElementById('closeButton');
-  const homeButton = document.getElementById('homeButton');
+document.addEventListener("DOMContentLoaded", function () {
+  const closeButton = document.getElementById("closeButton");
+  const homeButton = document.getElementById("homeButton");
   const languageButton = document.querySelector(".dropbtn");
-  const languageOptions = document.querySelectorAll('.dropup-content a');
+  const languageOptions = document.querySelectorAll(".dropup-content a");
 
   const savedLanguage = localStorage.getItem("selectedLanguage");
+
+  applyTranslation(savedLanguage);
 
   if (savedLanguage) {
     languageButton.textContent = savedLanguage;
   }
-
 
   // Function to change language
   function setLanguage(language) {
@@ -20,20 +19,42 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log("Language changed to:", language);
   }
 
-  closeButton.addEventListener('click', () => {
+  closeButton.addEventListener("click", () => {
     window.close();
   });
 
-  homeButton.addEventListener('click', () => {
-    window.location.href = 'popup.html';
+  homeButton.addEventListener("click", () => {
+    window.location.href = "popup.html";
   });
 
   languageOptions.forEach((option) => {
     option.addEventListener("click", function (event) {
       event.preventDefault(); // Prevent default anchor behavior
-  
+
       const selectedLanguage = option.textContent; // Get the selected language
       setLanguage(selectedLanguage); // Call the setLanguage function
     });
   });
+  
+  applyTranslation(savedLanguage);
 });
+
+const translations = {
+  English: {
+    language: "Language:",
+    machineTranslate: "Machine Translate:",
+  },
+  Arabic: {
+    language: "لغة:",
+    machineTranslate: "الترجمة الآلية:",
+  },
+  Swahili: {
+    language: "Lugha:",
+    machineTranslate: "Ufasiri wa mashine:",
+  },
+};
+function applyTranslation(language) {
+  const translation = translations[language];
+  document.getElementById("lang").textContent = translation.language;
+  document.getElementById("machineTranslate").textContent = translation.machineTranslate;
+}
